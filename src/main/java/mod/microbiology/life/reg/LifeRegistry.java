e62bb749480adac718a31e15e6d9de04e0e5c986
KaMiKaZe642 <kamikaze6424.doggames@gmail.com>
2015-10-11 23:23:37 +0200

    Ok... now LifeRegistry is entirely complete... for the moment

--- src/main/java/mod/microbiology/life/reg/LifeRegistry.java
+++ src/main/java/mod/microbiology/life/reg/LifeRegistry.java
@@ -3,6 +3,9 @@ package mod.microbiology.life.reg;
 import java.util.HashMap;
 import java.util.Random;
 
+import mod.microbiology.automata.DeterministicStateMachine;
+import mod.microbiology.automata.NondeterministicTable;
+import mod.microbiology.automata.TransitionTable;
 import mod.microbiology.life.IProperty;
 
 public class LifeRegistry {
@@ -10,12 +13,17 @@ public class LifeRegistry {
 	private static HashMap<String, HashMap<String, IProperty>> reg;
 	private static HashMap<String, String> forms;
 	
+	private static DeterministicStateMachine dsm_forms;
+	
+	private static HashMap<String, DeterministicStateMachine> machines;
+	
 	private static HashMap<Integer, Random> rngs;
 	
 	public LifeRegistry() {
 		reg = new HashMap<String, HashMap<String, IProperty>>();
 		forms = new HashMap<String, String>();
 		rngs = new HashMap<Integer, Random>();
+		machines = new HashMap<String, DeterministicStateMachine>();
 	}
 	
 	public String generateForm(int moduleid, String name) {
@@ -76,6 +84,26 @@ public class LifeRegistry {
 	
 	public String getForm(String pat) {
 		return forms.get(pat);
+	}
+	
+	public void setupAutomata() {
+		TransitionTable tt_1 = new TransitionTable();
+		new NondeterministicTable(forms.values().toArray(new String[]{}), tt_1);
+		dsm_forms = new DeterministicStateMachine(tt_1);
+		for (String s : forms.values()) {
+			TransitionTable tt_2 = new TransitionTable();
+			new NondeterministicTable(reg.get(s).keySet().toArray(new String[]{}), tt_2);
+			DeterministicStateMachine m = new DeterministicStateMachine(tt_2);
+			machines.put(s, m);
+		}
+	}
+	
+	public String getFormOfDNA(char[] dna) {
+		return dsm_forms.match(dna)[0];
+	}
+	
+	public String[] findProperties(String form, char[] dna) {
+		return machines.get(form).match(dna);
 	}
 	
 }
