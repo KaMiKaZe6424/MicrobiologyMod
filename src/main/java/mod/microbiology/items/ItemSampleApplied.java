d7840762523fc4940503a8faf0333d75e6f2a18f
Helibum1 <lukas.boner@stud.ksimlee.ch>
2015-10-11 18:38:35 +0200

    -Added the functionality to clear a sample

--- src/main/java/mod/microbiology/items/ItemSampleApplied.java
+++ src/main/java/mod/microbiology/items/ItemSampleApplied.java
@@ -3,7 +3,10 @@ package mod.microbiology.items;
 import java.util.List;
 
 import mod.microbiology.Microbiology;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
 
 public class ItemSampleApplied extends Item implements IItemSampleApplied {
 	
@@ -18,6 +21,22 @@ public class ItemSampleApplied extends Item implements IItemSampleApplied {
 	public List<char[]> getDNACodes() {
 		// TODO Auto-generated method stub
 		return null;
+	}
+	
+	@Override
+	public ItemStack onItemRightClick(ItemStack s, World w, EntityPlayer p) {
+		
+		if (p.isSneaking()) {
+			if (p.inventory.hasItem(new ItemSample())) {
+				p.inventory.addItemStackToInventory(new ItemStack(new ItemSample(), 1));
+				return new ItemStack(s.getItem(), s.stackSize-1);
+			} else {
+				return new ItemStack(new ItemSample(),1);
+			}
+		}
+		
+		return s;
+		
 	}
 	
 }
