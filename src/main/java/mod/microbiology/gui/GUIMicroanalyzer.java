28e6ced44f579144c577f76f41a89e0a35959fe8
Helibum1 <lukas.boner@stud.ksimlee.ch>
2016-01-18 13:35:09 +0100

    more gui

--- src/main/java/mod/microbiology/gui/GUIMicroanalyzer.java
+++ src/main/java/mod/microbiology/gui/GUIMicroanalyzer.java
@@ -5,15 +5,11 @@ import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.GuiScreen;
 
 public class GUIMicroanalyzer extends GuiScreen {
-	
-	public GUIMicroanalyzer() {
-		super();
-		this.drawCenteredString(Minecraft.getMinecraft().fontRenderer, "Placeholder GUI", 100, 100, 100);
-	}
+
 	
 	@Override
 	public void drawScreen(int p_73863_1_, int p_73863_2_, float p_73863_3_) {
-		// TODO Auto-generated method stub
+		this.drawCenteredString(Minecraft.getMinecraft().fontRenderer, "Placeholder GUI", 100, 100, 100);
 		super.drawScreen(p_73863_1_, p_73863_2_, p_73863_3_);
 	}
 	
